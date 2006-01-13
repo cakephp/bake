33217777d1f376bc617df644bf25c966947c44fc
phpnut <phpnut@cakephp.org>
2006-01-13 23:53:23 +0000

    Merging fixes to trunk
    Revision: [1796]
    Adding missing layout error messages.
    Changed more errors in View class to use the ErrorHandler class
    
    Revision: [1795]
    Added the missing error messages I found after RC 1 was built.
    Corrected errors in the View class when helpers where not found.
    Added error views for missing component classes and files.
    Updated views for missing view classes and files
    
    Revision: [1794]
    Adding fix for Ticket #273
    
    Revision: [1793]
    Correcting loading order of required files in bake.php
    
    Revision: [1792]
    Added fix for Ticket #271
    
    Revision: [1791]
    Fix for Ticket #268 and Ticket #272
    
    git-svn-id: https://svn.cakephp.org/repo/trunk/cake@1797 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/scripts/bake.php
+++ cake/scripts/bake.php
@@ -4,20 +4,20 @@
 
 /**
  * Bake startup script
- * 
+ *
  * Invokes the Bake class with given parameters.
  *
  * PHP versions 4 and 5
  *
  * CakePHP :  Rapid Development Framework <http://www.cakephp.org/>
- * Copyright (c) 2005, Cake Software Foundation, Inc. 
+ * Copyright (c) 2005, Cake Software Foundation, Inc.
  *                     1785 E. Sahara Avenue, Suite 490-204
  *                     Las Vegas, Nevada 89104
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @filesource 
+ * @filesource
  * @copyright    Copyright (c) 2005, Cake Software Foundation, Inc.
  * @link         http://www.cakefoundation.org/projects/info/cakephp CakePHP Project
  * @package      cake
@@ -36,8 +36,8 @@ define ('DS', DIRECTORY_SEPARATOR);
 define ('ROOT', dirname(dirname(dirname(__FILE__))).DS);
 define ('APP_DIR', 'app');
 
-require (ROOT.'cake'.DS.'config'.DS.'paths.php');
 require (ROOT.'cake'.DS.'basics.php');
+require (ROOT.'cake'.DS.'config'.DS.'paths.php');
 uses ('bake');
 
 $waste = array_shift($argv);
