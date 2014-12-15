8260fffed96b2ecf45ac1f3c7b73cfcaf4dabce3
AD7six <andydawson76@gmail.com>
2014-12-15 08:59:20 +0000

    Consistent @since tags
    Change them all to be relevant to this repo

--- tests/TestCase/Shell/Task/ModelTaskTest.php
+++ tests/TestCase/Shell/Task/ModelTaskTest.php
@@ -9,7 +9,7 @@
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP Project
- * @since         1.2.6
+ * @since         0.1.0
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace Bake\Test\TestCase\Shell\Task;
