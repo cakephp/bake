eb9ff7daa9401e0ee6f95c060b817153ad217994
Mark Story <mark@mark-story.com>
2015-02-09 21:00:23 -0500

    Fix PHPCS error.

--- src/Shell/Task/TestTask.php
+++ src/Shell/Task/TestTask.php
@@ -201,7 +201,7 @@ class TestTask extends BakeTask
 
         $baseNamespace = Configure::read('App.namespace');
         if ($this->plugin) {
-            $baseNamespace = $this->_pluginNamespace($this->plugin);            
+            $baseNamespace = $this->_pluginNamespace($this->plugin);
         }
         $subNamespace = substr($namespace, strlen($baseNamespace) + 1);
 
