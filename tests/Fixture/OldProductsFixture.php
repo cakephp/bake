139185f85139c2e5b49f31c67547846d81e846ed
ionas <jh@ht-studios.de>
2015-12-31 20:17:38 +0000

    improving tests, still failing :/

--- tests/Fixture/OldProductsFixture.php
+++ tests/Fixture/OldProductsFixture.php
@@ -1,16 +1,27 @@
 <?php
-namespace App\Test\Fixture;
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         1.1.4
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Bake\Test\Fixture;
 
 use Cake\TestSuite\Fixture\TestFixture;
 
 /**
- * AssociationDetectionCategoriesFixture
+ * OldProductsFixture
  *
  */
-class AssociationDetectionCategoriesFixture extends TestFixture
+class OldProductsFixture extends TestFixture
 {
-
-    public $table = 'categories';
 
     /**
      * Fields
@@ -21,7 +32,7 @@ class AssociationDetectionCategoriesFixture extends TestFixture
     public $fields = [
         'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
         'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
-        'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        'modified' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
         'name' => ['type' => 'string', 'length' => 100, 'null' => false, 'default' => '', 'comment' => '', 'precision' => null, 'fixed' => null],
         '_constraints' => [
             'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
--- tests/Fixture/ProductVersionsFixture.php
+++ tests/Fixture/ProductVersionsFixture.php
@@ -1,16 +1,27 @@
 <?php
-namespace App\Test\Fixture;
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         1.1.4
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Bake\Test\Fixture;
 
 use Cake\TestSuite\Fixture\TestFixture;
 
 /**
- * AssociationDetectionProductVersionsFixture
+ * ProductVersionsFixture
  *
  */
-class AssociationDetectionProductVersionsFixture extends TestFixture
+class ProductVersionsFixture extends TestFixture
 {
-
-    public $table = 'product_versions';
 
     /**
      * Fields
