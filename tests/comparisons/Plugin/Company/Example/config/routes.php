40b77c90c17b8c957e768dae78c5b5502fa67f33
Mark Story <mark@mark-story.com>
2016-09-02 22:35:28 -0400

    Fix failing tests.
    These squeaked by because of poor code review.

--- tests/comparisons/Plugin/Company/Example/config/routes.php
+++ tests/comparisons/Plugin/Company/Example/config/routes.php
@@ -1,11 +1,12 @@
 <?php
 use Cake\Routing\RouteBuilder;
+use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\Router;
 
 Router::plugin(
     'Company/Example',
     ['path' => '/company/example'],
     function (RouteBuilder $routes) {
-        $routes->fallbacks('DashedRoute');
+        $routes->fallbacks(DashedRoute::class);
     }
 );
--- tests/comparisons/Plugin/SimpleExample/config/routes.php
+++ tests/comparisons/Plugin/SimpleExample/config/routes.php
@@ -1,11 +1,12 @@
 <?php
 use Cake\Routing\RouteBuilder;
+use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\Router;
 
 Router::plugin(
     'SimpleExample',
     ['path' => '/simple-example'],
     function (RouteBuilder $routes) {
-        $routes->fallbacks('DashedRoute');
+        $routes->fallbacks(DashedRoute::class);
     }
 );
