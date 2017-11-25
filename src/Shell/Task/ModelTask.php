45d0bfc97d762748f5bf58f97f9ec76f588d630b
Mark Sch <dereuromark@users.noreply.github.com>
2017-11-25 11:23:03 +0100

    Revert "Remove argument typehint."

--- src/Shell/Task/ModelTask.php
+++ src/Shell/Task/ModelTask.php
@@ -694,7 +694,7 @@ class ModelTask extends BakeTask
      * @param array $primaryKey The primary key field
      * @return array Array of validation for the field.
      */
-    public function fieldValidation($schema, $fieldName, array $metaData, $primaryKey)
+    public function fieldValidation($schema, $fieldName, array $metaData, array $primaryKey)
     {
         $ignoreFields = ['lft', 'rght', 'created', 'modified', 'updated'];
         if (in_array($fieldName, $ignoreFields)) {
