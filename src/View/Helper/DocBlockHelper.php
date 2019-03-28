932ba4b59ec82d08e7adb9c89d6278d86d97ce9d
Marc Würth <ravage@bluewin.ch>
2019-03-28 01:37:14 +0100

    Rename annotationSpacing property in DocBlockHelper to clarify its use

--- src/View/Helper/DocBlockHelper.php
+++ src/View/Helper/DocBlockHelper.php
@@ -16,7 +16,7 @@ class DocBlockHelper extends Helper
     /**
      * @var bool Whether to add a blank line between different class annotations
      */
-    protected $_annotationSpacing = true;
+    protected $_classAnnotationSpacing = true;
 
     /**
      * Writes the DocBlock header for a class which includes the property and method declarations. Annotations are
@@ -42,7 +42,7 @@ class DocBlockHelper extends Helper
         foreach ($annotations as $annotation) {
             if (strlen($annotation) > 1 && $annotation[0] === '@' && strpos($annotation, ' ') > 0) {
                 $type = substr($annotation, 0, strpos($annotation, ' '));
-                if ($this->_annotationSpacing &&
+                if ($this->_classAnnotationSpacing &&
                     $previous !== false &&
                     $previous !== $type
                 ) {
