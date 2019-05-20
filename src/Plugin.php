2459485b6cfdc28fd772d2b7dcd3cf6e15a7be39
Mark Story <mark@mark-story.com>
2019-05-19 22:54:40 -0400

    Remove double declare()

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,7 +1,6 @@
 <?php
 declare(strict_types=1);
 
-declare(strict_types=1);
 /**
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
