618507c24c6f51ad73653a0ad21fbfd363a07c03
Mark Story <mark@mark-story.com>
2019-05-22 23:20:55 -0400

    Update test comparison files

--- tests/comparisons/Template/testGetContentAssociations.php
+++ tests/comparisons/Template/testGetContentAssociations.php
@@ -4,31 +4,33 @@
  * @var \Bake\Test\App\Model\Entity\TemplateTaskComment $templateTaskComment
  */
 ?>
-<nav class="large-3 medium-4 columns" id="actions-sidebar">
-    <ul class="side-nav">
-        <li class="heading"><?= __('Actions') ?></li>
-        <li><?= $this->Html->link(__('Edit Template Task Comment'), ['action' => 'edit', $templateTaskComment->id]) ?> </li>
-        <li><?= $this->Form->postLink(__('Delete Template Task Comment'), ['action' => 'delete', $templateTaskComment->id], ['confirm' => __('Are you sure you want to delete # {0}?', $templateTaskComment->id)]) ?> </li>
-        <li><?= $this->Html->link(__('List Template Task Comments'), ['action' => 'index']) ?> </li>
-        <li><?= $this->Html->link(__('New Template Task Comment'), ['action' => 'add']) ?> </li>
-        <li><?= $this->Html->link(__('List Authors'), ['controller' => 'TemplateTaskAuthors', 'action' => 'index']) ?> </li>
-        <li><?= $this->Html->link(__('New Author'), ['controller' => 'TemplateTaskAuthors', 'action' => 'add']) ?> </li>
-    </ul>
-</nav>
-<div class="templateTaskComments view large-9 medium-8 columns content">
-    <h3><?= h($templateTaskComment->name) ?></h3>
-    <table class="vertical-table">
-        <tr>
-            <th scope="row"><?= __('Name') ?></th>
-            <td><?= h($templateTaskComment->name) ?></td>
-        </tr>
-        <tr>
-            <th scope="row"><?= __('Body') ?></th>
-            <td><?= h($templateTaskComment->body) ?></td>
-        </tr>
-        <tr>
-            <th scope="row"><?= __('Id') ?></th>
-            <td><?= $this->Number->format($templateTaskComment->id) ?></td>
-        </tr>
-    </table>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('Edit Template Task Comment'), ['action' => 'edit', $templateTaskComment->id], ['class' => 'side-nav-item']) ?>
+            <?= $this->Form->postLink(__('Delete Template Task Comment'), ['action' => 'delete', $templateTaskComment->id], ['confirm' => __('Are you sure you want to delete # {0}?', $templateTaskComment->id), 'class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('List Template Task Comments'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('New Template Task Comment'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="templateTaskComments view content">
+            <h3><?= h($templateTaskComment->name) ?></h3>
+            <table>
+                <tr>
+                    <th><?= __('Name') ?></th>
+                    <td><?= h($templateTaskComment->name) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Body') ?></th>
+                    <td><?= h($templateTaskComment->body) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Id') ?></th>
+                    <td><?= $this->Number->format($templateTaskComment->id) ?></td>
+                </tr>
+            </table>
+        </div>
+    </div>
 </div>
--- tests/comparisons/Template/testGetContentWithRoutingPrefix-add.php
+++ tests/comparisons/Template/testGetContentWithRoutingPrefix-add.php
@@ -4,21 +4,25 @@
  * @var \Bake\Test\App\Model\Entity\TestTemplateModel $testTemplateModel
  */
 ?>
-<nav class="large-3 medium-4 columns" id="actions-sidebar">
-    <ul class="side-nav">
-        <li class="heading"><?= __('Actions') ?></li>
-        <li><?= $this->Html->link(__('List Test Template Models'), ['action' => 'index']) ?></li>
-    </ul>
-</nav>
-<div class="testTemplateModels form large-9 medium-8 columns content">
-    <?= $this->Form->create($testTemplateModel) ?>
-    <fieldset>
-        <legend><?= __('Add Test Template Model') ?></legend>
-        <?php
-            echo $this->Form->control('name');
-            echo $this->Form->control('body');
-        ?>
-    </fieldset>
-    <?= $this->Form->button(__('Submit')) ?>
-    <?= $this->Form->end() ?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('List Test Template Models'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="testTemplateModels form content">
+            <?= $this->Form->create($testTemplateModel) ?>
+            <fieldset>
+                <legend><?= __('Add Test Template Model') ?></legend>
+                <?php
+                    echo $this->Form->control('name');
+                    echo $this->Form->control('body');
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
 </div>
