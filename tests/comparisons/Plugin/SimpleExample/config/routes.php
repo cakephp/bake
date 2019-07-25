65a760ab42d80d3b10819ffa176722efc14f8f67
ADmad <admad.coder@gmail.com>
2019-07-25 20:41:22 +0530

    Add inline @var tag.

--- tests/comparisons/Plugin/SimpleExample/config/routes.php
+++ tests/comparisons/Plugin/SimpleExample/config/routes.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
 
+/** @var \Cake\Routing\RouteBuilder $routes */
 $routes->plugin(
     'SimpleExample',
     ['path' => '/simple-example'],
