415d7aee419f79fc67c343b7fb7119ead6bb8b36
ADmad <admad.coder@gmail.com>
2019-07-29 18:08:03 +0530

    Use Plugin methods for bootstrapping and setting routes.
    Avoid loading separate files.
    
    Closes #575.

--- tests/comparisons/Plugin/Company/Example/src/Plugin.php
+++ tests/comparisons/Plugin/Company/Example/src/Plugin.php
@@ -4,10 +4,43 @@ declare(strict_types=1);
 namespace Company\Example;
 
 use Cake\Core\BasePlugin;
+use Cake\Core\PluginApplicationInterface;
+use Cake\Routing\RouteBuilder;
 
 /**
  * Plugin for Company\Example
  */
 class Plugin extends BasePlugin
 {
+    /**
+     * Load all the plugin configuration and bootstrap logic.
+     *
+     * The host application is provided as an argument. This allows you to load
+     * additional plugin dependencies, or attach events.
+     *
+     * @param \Cake\Core\PluginApplicationInterface $app The host application
+     * @return void
+     */
+    public function bootstrap(PluginApplicationInterface $app): void
+    {
+    }
+
+    /**
+     * Add routes for the plugin.
+     *
+     * @param \Cake\Routing\RouteBuilder $routes The route builder to update.
+     * @return void
+     */
+    public function routes(RouteBuilder $routes): void
+    {
+        $routes->plugin(
+            'Company/Example',
+            ['path' => '/company/example'],
+            function (RouteBuilder $builder) {
+                // Add custom routes here
+
+                $builder->fallbacks();
+            }
+        );
+    }
 }
--- tests/comparisons/Plugin/SimpleExample/src/Plugin.php
+++ tests/comparisons/Plugin/SimpleExample/src/Plugin.php
@@ -4,10 +4,43 @@ declare(strict_types=1);
 namespace SimpleExample;
 
 use Cake\Core\BasePlugin;
+use Cake\Core\PluginApplicationInterface;
+use Cake\Routing\RouteBuilder;
 
 /**
  * Plugin for SimpleExample
  */
 class Plugin extends BasePlugin
 {
+    /**
+     * Load all the plugin configuration and bootstrap logic.
+     *
+     * The host application is provided as an argument. This allows you to load
+     * additional plugin dependencies, or attach events.
+     *
+     * @param \Cake\Core\PluginApplicationInterface $app The host application
+     * @return void
+     */
+    public function bootstrap(PluginApplicationInterface $app): void
+    {
+    }
+
+    /**
+     * Add routes for the plugin.
+     *
+     * @param \Cake\Routing\RouteBuilder $routes The route builder to update.
+     * @return void
+     */
+    public function routes(RouteBuilder $routes): void
+    {
+        $routes->plugin(
+            'SimpleExample',
+            ['path' => '/simple-example'],
+            function (RouteBuilder $builder) {
+                // Add custom routes here
+
+                $builder->fallbacks();
+            }
+        );
+    }
 }
