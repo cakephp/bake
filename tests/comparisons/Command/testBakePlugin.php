99d2ddc173a41cda419103f5f41710f1f186c7be
Mark Story <mark@mark-story.com>
2019-11-07 12:02:21 +0900

    Update bake template to remove typehint.
    Refs cakephp/cakephp#13852

--- tests/comparisons/Command/testBakePlugin.php
+++ tests/comparisons/Command/testBakePlugin.php
@@ -35,7 +35,7 @@ class ExampleCommand extends Command
      * @param \Cake\Console\ConsoleIo $io The console io
      * @return null|int The exit code or null for success
      */
-    public function execute(Arguments $args, ConsoleIo $io): ?int
+    public function execute(Arguments $args, ConsoleIo $io)
     {
     }
 }
