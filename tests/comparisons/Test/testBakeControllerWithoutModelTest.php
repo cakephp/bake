3f8a377b277018b0b95ce404a1d26dd6fef966fc
mscherer <mark.scherer@spryker.com>
2020-03-16 19:00:28 +0100

    Add test case.

---
+++ tests/comparisons/Test/testBakeControllerWithoutModelTest.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Bake\Test\App\Test\TestCase\Controller;
+
+use Bake\Test\App\Controller\NoModelController;
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * Bake\Test\App\Controller\NoModelController Test Case
+ *
+ * @uses \Bake\Test\App\Controller\NoModelController
+ */
+class NoModelControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex(): void
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
---
+++ tests/test_app/App/Controller/NoModelController.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link      http://cakephp.org CakePHP(tm) Project
+ * @since     0.1.0
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Bake\Test\App\Controller;
+
+/**
+ * NoModelController class
+ */
+class NoModelController extends AppController
+{
+    /**
+     * @var string
+     */
+    protected $modelClass = '';
+
+    /**
+     * Index method.
+     *
+     * @return void
+     */
+    public function index()
+    {
+        $this->set('test', 'value');
+    }
+}
