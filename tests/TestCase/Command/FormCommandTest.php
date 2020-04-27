4b7ef4390a135c6e27acfb3fa90014844ad0c05d
Corey Taylor <corey.taylor.fl@gmail.com>
2020-04-26 19:07:39 -0500

    Added FormCommandTest

---
+++ tests/TestCase/Command/FormCommandTest.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         2.1.1
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Bake\Test\TestCase\Command;
+
+use Bake\Test\TestCase\TestCase;
+use Cake\Command\Command;
+
+/**
+ * FormCommandTest class
+ */
+class FormCommandTest extends TestCase
+{
+    /**
+     * setup method
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->useCommandRunner();
+        $this->setAppNamespace('Bake\Test\App');
+    }
+
+    /**
+     * Tests the form command.
+     *
+     * @return void
+     */
+    public function testCommand()
+    {
+        $this->generatedFiles = [
+            APP . 'Form' . DS . 'TestForm.php',
+        ];
+        $this->exec('bake form Test', ['y']);
+
+        $this->assertExitCode(Command::CODE_SUCCESS);
+        $this->assertFilesExist($this->generatedFiles);
+        $this->assertFileContains('class TestForm extends Form', $this->generatedFiles[0]);
+    }
+}
