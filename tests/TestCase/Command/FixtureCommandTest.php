7a4a410dd4e8e87e09e7854342b5996f1dfbfa8b
Mark Story <mark@mark-story.com>
2021-09-03 16:56:16 -0400

    Fix phpcs

--- tests/TestCase/Command/FixtureCommandTest.php
+++ tests/TestCase/Command/FixtureCommandTest.php
@@ -238,7 +238,7 @@ class FixtureCommandTest extends TestCase
         $this->assertStringContainsString("'columns' => ['id']", $result);
         $this->assertStringContainsString("'uuid' => ['type' => 'uuid'", $result);
         $this->assertRegExp(
-            "/(\s+)('uuid' => ')([a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})(')/", 
+            "/(\s+)('uuid' => ')([a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})(')/",
             $result
         );
     }
