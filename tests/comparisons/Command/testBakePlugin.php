a327ef119826113bb6a97ec30114e26a3b719829
ADmad <admad.coder@gmail.com>
2022-07-06 19:04:55 +0530

    Update command's template.

--- tests/comparisons/Command/testBakePlugin.php
+++ tests/comparisons/Command/testBakePlugin.php
@@ -17,8 +17,8 @@ class ExampleCommand extends Command
      * Hook method for defining this command's option parser.
      *
      * @see https://book.cakephp.org/4/en/console-commands/commands.html#defining-arguments-and-options
-     * @param \Cake\Console\ConsoleOptionParser $parser The parser to be defined
-     * @return \Cake\Console\ConsoleOptionParser The built parser.
+     * @param \Cake\Console\ConsoleOptionParser $parser The parser to be defined.
+     * @return \Cake\Console\ConsoleOptionParser
      */
     public function buildOptionParser(ConsoleOptionParser $parser): ConsoleOptionParser
     {
@@ -31,10 +31,11 @@ class ExampleCommand extends Command
      * Implement this method with your command's logic.
      *
      * @param \Cake\Console\Arguments $args The command arguments.
-     * @param \Cake\Console\ConsoleIo $io The console io
-     * @return null|void|int The exit code or null for success
+     * @param \Cake\Console\ConsoleIo $io The console io.
+     * @return int The exit code.
      */
-    public function execute(Arguments $args, ConsoleIo $io)
+    public function execute(Arguments $args, ConsoleIo $io): int
     {
+        return static::CODE_SUCCESS;
     }
 }
