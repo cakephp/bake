63a70bd28da4bf5265e637b47bb9da32e744be98
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2022-07-16 16:47:28 +0200

    fix phpdoc

--- src/Command/ModelCommand.php
+++ src/Command/ModelCommand.php
@@ -811,7 +811,7 @@ class ModelCommand extends BakeCommand
      * @param \Cake\Database\Schema\TableSchemaInterface $schema The table schema for the current field.
      * @param string $fieldName Name of field to be validated.
      * @param array $metaData metadata for field
-     * @param array<string> $primaryKey The primary key field. Unused because PK validation is skippe
+     * @param array<string> $primaryKey The primary key field. Unused because PK validation is skipped
      * @return array Array of validation for the field.
      */
     public function fieldValidation(
