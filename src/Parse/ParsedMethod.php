318216f27a1f2219e1a46a845e5fda8b8b2a63c8
Corey Taylor <corey.taylor.fl@gmail.com>
2022-07-15 20:30:40 -0500

    Add parser to find existing class details for non-destructive baking

---
+++ src/Parse/ParsedMethod.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         3.0.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Bake\Parse;
+
+/**
+ * @internal
+ */
+final class ParsedMethod
+{
+    /**
+     * @param string $name name
+     * @param string $code Code block
+     * @param string|null $docblock Docblock
+     * @param array $attributes Attributes
+     */
+    public function __construct(
+        public readonly string $name,
+        public readonly string $code,
+        public readonly ?string $docblock,
+        public readonly array $attributes
+    ) {
+    }
+}
