b38aab930af26ee1ad49768cbc340abefe46bde9
mscherer <dereuromark@web.de>
2023-12-31 17:07:23 +0100

    Stricter validation

--- src/Utility/Model/EnumParser.php
+++ src/Utility/Model/EnumParser.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Bake\Utility\Model;
 
+use InvalidArgumentException;
+
 enum EnumParser
 {
     /**
@@ -26,6 +28,13 @@ enum EnumParser
                 $case = mb_substr($case, 0, strpos($case, ':'));
             } elseif ($int) {
                 $value = $k;
+            }
+
+            if (!preg_match('/^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*$/', $case)) {
+                throw new InvalidArgumentException(sprintf('`%s` is not a valid enum case', $case));
+            }
+            if (is_string($value) && str_contains($value, '\'')) {
+                throw new InvalidArgumentException(sprintf('`%s` value cannot contain `\'` character', $case));
             }
 
             $definition[$case] = $int ? (int)$value : $value;
