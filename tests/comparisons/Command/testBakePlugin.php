293a7de96e6ab05e158760a75e27199ac0a2788e
mscherer <dereuromark@web.de>
2025-07-22 19:45:56 +0200

    Consolidate links.

--- tests/comparisons/Command/testBakePlugin.php
+++ tests/comparisons/Command/testBakePlugin.php
@@ -43,7 +43,7 @@ class ExampleCommand extends Command
     /**
      * Hook method for defining this command's option parser.
      *
-     * @see https://book.cakephp.org/5/en/console-commands/commands.html#defining-arguments-and-options
+     * @link https://book.cakephp.org/5/en/console-commands/commands.html#defining-arguments-and-options
      * @param \Cake\Console\ConsoleOptionParser $parser The parser to be defined
      * @return \Cake\Console\ConsoleOptionParser The built parser.
      */
