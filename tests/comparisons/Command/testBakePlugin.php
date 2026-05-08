45139a702f039b64dcfb758bff60910861486060
ADmad <admad.coder@gmail.com>
2026-05-08 19:26:13 +0530

    Fix command name. (#1082)
    As seen in the default value for `BaseCommand::$name` and asserted
    in `BaseCommand::setName()`, the command names need a "root" part
    which is usually "cake".

--- tests/comparisons/Command/testBakePlugin.php
+++ tests/comparisons/Command/testBakePlugin.php
@@ -18,7 +18,7 @@ class ExampleCommand extends Command
      *
      * @var string
      */
-    protected string $name = 'test_bake example';
+    protected string $name = 'cake test_bake example';
 
     /**
      * Get the default command name.
