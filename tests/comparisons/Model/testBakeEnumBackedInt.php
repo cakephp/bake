bd95e08533a2142c58a7f7edf81de1c38c849e42
ADmad <admad.coder@gmail.com>
2026-06-05 18:10:24 +0530

    Generate enums with EnumLabelTrait

--- tests/comparisons/Model/testBakeEnumBackedInt.php
+++ tests/comparisons/Model/testBakeEnumBackedInt.php
@@ -4,18 +4,12 @@ declare(strict_types=1);
 namespace Bake\Test\App\Model\Enum;
 
 use Cake\Database\Type\EnumLabelInterface;
-use Cake\Utility\Inflector;
+use Cake\Database\Type\EnumLabelTrait;
 
 /**
  * FooBar Enum
  */
 enum FooBar: int implements EnumLabelInterface
 {
-    /**
-     * @return string
-     */
-    public function label(): string
-    {
-        return Inflector::humanize(Inflector::underscore($this->name));
-    }
+    use EnumLabelTrait;
 }
--- tests/test_app/App/Model/Enum/BakeUserNullableGender.php
+++ tests/test_app/App/Model/Enum/BakeUserNullableGender.php
@@ -4,22 +4,16 @@ declare(strict_types=1);
 namespace Bake\Test\App\Model\Enum;
 
 use Cake\Database\Type\EnumLabelInterface;
-use Cake\Utility\Inflector;
+use Cake\Database\Type\EnumLabelTrait;
 
 /**
  * BakeUserNullableGender Enum
  */
 enum BakeUserNullableGender: string implements EnumLabelInterface
 {
+    use EnumLabelTrait;
+
     case Male = 'male';
     case Female = 'female';
     case Diverse = 'diverse';
-
-    /**
-     * @return string
-     */
-    public function label(): string
-    {
-        return Inflector::humanize(Inflector::underscore($this->name));
-    }
 }
